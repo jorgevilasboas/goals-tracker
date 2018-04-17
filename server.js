23f6a44bd9fbda2146f2eae1418a804f1ea493d7
jorgevilasboas <jorge.sergipe@gmail.com>
2018-04-17 10:43:31 -0300

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,46 @@
-const express = require('express'),
-      app = express(),
-      port = Number(process.env.PORT || 8080);
+// server.js
 
-app.get('/', (req,res) =>{
-  res.send('Our first route is working.:)');	
+// DEPENDENCIES AND SETUP
+// ===============================================
+
+var express = require('express'),
+  app = express(),
+  port = Number(process.env.PORT || 8080);
+
+// DATABASE
+// ===============================================
+
+// Setup the database.
+var Datastore = require('nedb');
+var db = new Datastore({
+  filename: 'goals.db', // provide a path to the database file 
+  autoload: true, // automatically load the database
+  timestampData: true // automatically add and manage the fields createdAt and updatedAt
 });
 
-app.listen(port, ()=>{
-	console.log('Listening on port' + port);
+// Let us check that we can save to the database.
+// Define a goal.
+var goal = {
+  description: 'Do 10 minutes meditation every day',
+};
+
+// Save this goal to the database.
+db.insert(goal, function(err, newGoal) {
+  if (err) console.log(err);
+  console.log(newGoal);
+});
+
+// ROUTES
+// ===============================================
+
+// Define the home page route.
+app.get('/', function(req, res) {
+  res.send('Our first route is working. :)');
+});
+
+// START THE SERVER
+// ===============================================
+
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
 });
