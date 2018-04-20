fbef143cef01800e91caf79bfaf3b5a1bfdc0b6b
jorgevilasboas <jorge.sergipe@gmail.com>
2018-04-20 16:45:34 -0300

    Adds rest

--- server.js
+++ server.js
@@ -5,8 +5,65 @@
 
 var express = require('express'),
   app = express(),
+  bodyParser = require('body-parser'),
   port = Number(process.env.PORT || 8080);
 
+// Set up body-parser.
+// To parse JSON:
+app.use(bodyParser.json());
+// To parse form data:
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// GET all goals.
+// (Accessed at GET http://localhost:8080/goals)
+app.get('/goals', function(req, res) {
+  db.find({}).sort({
+    updatedAt: -1
+  }).exec(function(err, goals) {
+    if (err) res.send(err);
+    res.json(goals);
+  });
+});
+
+// POST a new goal.
+// (Accessed at POST http://localhost:8080/goals)
+app.post('/goals', function(req, res) {
+  var goal = {
+    description: req.body.description,
+  };
+  db.insert(goal, function(err, goal) {
+    if (err) res.send(err);
+    res.json(goal);
+  });
+});
+
+// GET a goal.
+// (Accessed at GET http://localhost:8080/goals/goal_id)
+app.get('/goals/:id', function(req, res) {
+  var goal_id = req.params.id;
+  db.findOne({
+    _id: goal_id
+  }, {}, function(err, goal) {
+    if (err) res.send(err);
+    res.json(goal);
+  });
+});
+
+
+// DELETE a goal.
+// (Accessed at DELETE http://localhost:8080/goals/goal_id)
+app.delete('/goals/:id', function(req, res) {
+  var goal_id = req.params.id;
+  db.remove({
+    _id: goal_id
+  }, {}, function(err, goal) {
+    if (err) res.send(err);
+    res.sendStatus(200);
+  });
+});
+  
 // DATABASE
 // ===============================================
 
